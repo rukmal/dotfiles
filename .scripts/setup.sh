f2ff52e3722ea20952119b3e6e0efdd4061c830f
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2015-03-04 23:51:56 -0800

    Removed node-based installations

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -2,12 +2,13 @@
 # Script to setup the dotfiles, and put everything in the correct place
 # This script assumes the repository has been cloned into the 'dotfiles'
 # directory in the home folder
-# WARNING: This script assumes ST3, iTerm2, and Node.js are installed
+# WARNING: This script assumes ST3 and iTerm2 are installed
 # NOTE: Only works on Mac OS X
 #======================================================================
 
 echo "Moving all dotfiles to the home directory..."
 mv ~/dotfiles/.* .. &> /dev/null
+rm -r ~/dotfiles/
 echo $'Done...\n'
 
 echo "Copying iTerm2 preferences to ~/Libaray/Preferences/"
@@ -16,11 +17,6 @@ echo $'Done...\n'
 
 echo "Copying Sublime Text 3 Preferences to ~/Library/Application Support/Sublime Text 3/Packages/User/"
 cp ~/.st3/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-echo $'Done...\n'
-
-echo "Installing select Node-based dependencies..."
-sudo npm install -g license-generator
-sudo npm install -g grunt-cli
 echo $'Done...\n'
 
 echo "Operation completed successfully."
