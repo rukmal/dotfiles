9c009d1fa8d6d9dc46e4e7931cfe261e2d6cee81
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-01-11 09:54:00 +0530

    Added autocomplete for Kubernetes' kubectl

--- .terminal/applications.bash
+++ .terminal/applications.bash
@@ -4,3 +4,11 @@
 export PATH="/Users/rukmal/Applications/anaconda3/bin:$PATH"  # Command line utility
 
 . /Users/rukmal/Applications/anaconda3/etc/profile.d/conda.sh  # Enabling 'conda activate'
+
+
+# Kubernetes
+#============
+
+if [ $commands[kubectl] ]; then
+    source <(kubectl completion zsh);
+fi
