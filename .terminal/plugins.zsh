f702f23267e850fa8b68af1ff5ede85363aa73e7
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-03-20 02:55:36 -0400

    Major overhaul. Refactored everything, and switched to antigen for zsh management

---
+++ .terminal/plugins.zsh
@@ -0,0 +1,22 @@
+# Plugin stuff
+
+# Autocompletion plugins
+antigen bundle git # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#git
+antigen bundle heroku # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#heroku
+antigen bundle pip # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#pip
+antigen bundle brew # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#brew
+antigen bundle kubectl # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#kubectl
+antigen bundle python # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#python
+antigen bundle docker # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#docker
+antigen bundle extract # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#extract
+antigen bundle osx # https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/osx
+antigen bundle jsontools # https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#jsontools
+# antigen bundle zsh-users/zsh-syntax-highlighting 
+
+# zsh-nvm plugin
+# See: https://github.com/lukechilds/zsh-nvm
+export NVM_LAZY_LOAD=true  # Do not load on start, only do so when called
+antigen bundle lukechilds/zsh-nvm
+
+# Done with antigen plugin stuff, apply
+antigen apply
