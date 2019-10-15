a705b635028bab6a138e47036c82e15440a1858d
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-10-14 23:57:13 -0700

    Update with EH MacBook Air prefs

--- .terminal/applications.bash
+++ .terminal/applications.bash
@@ -1,9 +1,9 @@
 # Anaconda 3
 #============
 
-export PATH="/Users/rukmal/Applications/anaconda3/bin:$PATH"  # Command line utility
+export PATH="/Users/rukmalw/Applications/anaconda3/bin:$PATH"  # Command line utility
 
-. /Users/rukmal/Applications/anaconda3/etc/profile.d/conda.sh  # Enabling 'conda activate'
+. /Users/rukmalw/Applications/anaconda3/etc/profile.d/conda.sh  # Enabling 'conda activate'
 
 # Ruby
 #=====
